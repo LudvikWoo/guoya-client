55463d379a568db3939fc77dc5209f32add9ba85
LudvikWoo <LudvikWoo@users.noreply.github.com>
2018-03-22 08:44:03 +0800

    重写资料导航菜单

--- src/main/webapp/basicClass/js/guoya.js
+++ src/main/webapp/basicClass/js/guoya.js
@@ -1,3 +1,3 @@
 //var publicUrl = "http://192.168.1.45:8083";
-var publicUrl = "http://www.guoyasoft.com:15021";
-// var publicUrl="http://127.0.0.1:8083";
+//var publicUrl = "http://www.guoyasoft.com:15021";
+ var publicUrl="http://127.0.0.1:8083";
