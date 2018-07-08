7c541eb2bc9770a9f98f2616ffddd631481f4c8d
LudvikWoo <LudvikWoo@users.noreply.github.com>
2018-07-08 09:56:52 +0800

    修复Stringtools的bug

--- src/main/java/com/guoyasoft/tools/StringTools.java
+++ src/main/java/com/guoyasoft/tools/StringTools.java
@@ -2,10 +2,7 @@ package com.guoyasoft.tools;
 
 public class StringTools {
 	public static boolean isNotBlank(String str){
-		if(str != null){
-			str=str.trim();
-		}
-		if(!"".equals(str.trim())&& str.trim()!=null && !"null".equalsIgnoreCase(str.trim())){
+		if(str != null && !"".equals(str.trim()) && !"null".equalsIgnoreCase(str.trim())){
 			return true;
 		}else{
 			return false;
